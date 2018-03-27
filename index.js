c4b56e0cc98ebc3ffa9c2ed65c837e76f9e1eb33
Mark Sauer-Utley <mark.utley.713@gmail.com>
2018-03-27 11:45:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments){
   var sentences = [];
-  for(i = 0; i < musicians.length; i++){
+  for(i = 0; i < musicians.length; i++) {
     newSentence = musicians[i] + " plays " + instruments[i] + ".";
     sentences.push(newSentence);
   }
