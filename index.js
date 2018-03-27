0860e2b75c7c52fe7d895bd0b73abf9e888b3239
Mark Sauer-Utley <mark.utley.713@gmail.com>
2018-03-27 12:04:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function johnLennonFacts(facts){
   var exclamation = []; 
   while (i < facts.length){
     var exclaimed = facts[i] + "!!!";
-    exclamation.push(exclaimed);
+    exclamation[i] = exclaimed;
     i++;
   }
   return exclamation;
